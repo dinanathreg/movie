1cb95e69b567a6f8d605a69224fd8a806061ed56
dinanathreg <111105152+dinanathreg@users.noreply.github.com>
2022-08-12 11:38:00 +0545

    movie

---
+++ movie.js
@@ -0,0 +1,34 @@
+export default[
+
+  {
+    id:,
+    movie_name:"",
+    movie_thumnail:"",
+    movie_link:"",    
+  },
+  {
+    id:4,
+    movie_name:"",
+    movie_thumnail:"",
+    movie_link:"",    
+  },
+    {
+    id:3,
+    movie_name:"",
+    movie_thumnail:"",
+    movie_link:"",    
+  },
+    {
+    id:2,
+    movie_name:"",
+    movie_thumnail:"",
+    movie_link:"",    
+  },
+    {
+    id:1,
+    movie_name:"3 idot",
+    movie_thumnail:"",
+    movie_link:"",    
+  },
+  
+]
