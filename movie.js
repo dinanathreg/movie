9e9c0f18cb1663e326d63f859bc3b78e6251485b
dinanathreg <111105152+dinanathreg@users.noreply.github.com>
2022-08-12 11:57:21 +0545

    26

--- movie.js
+++ movie.js
@@ -14,27 +14,27 @@ export default[
   },
   {
     id:4,
-    movie_name:"",
-    movie_thumnail:"",
-    movie_link:"",    
+    movie_name:"iron man 2",
+    movie_thumnail:"https://img.fruugo.com/product/8/93/14220938_max.jpg",
+    movie_link:"https://new1.gdtot.sbs/file/9386713613",    
   },
     {
     id:3,
-    movie_name:"",
-    movie_thumnail:"",
-    movie_link:"",    
+    movie_name:"iron man 1",
+    movie_thumnail:"https://www.themoviedb.org/t/p/original/aGsjgTNMSF0kswDx57YCVPqCivW.jpg",
+    movie_link:"https://new1.gdtot.sbs/file/13573168983",    
   },
     {
     id:2,
-    movie_name:"",
-    movie_thumnail:"",
-    movie_link:"",    
+    movie_name:"Thor 2 the dark world",
+    movie_thumnail:"https://media.comicbook.com/wp-content/uploads/2013/09/Thor_Un_Mundo_Oscuro_Poster_Latino_ex_JPosters.jpg",
+    movie_link:"https://new1.gdtot.sbs/file/2412126687",    
   },
     {
     id:1,
     movie_name:"3 idot",
-    movie_thumnail:"",
-    movie_link:"",    
+    movie_thumnail:"https://www.themoviedb.org/t/p/original/o2MnmBooLMwcd7qdBCEyLQQqg9h.jpg",
+    movie_link:"https://new1.gdtot.sbs/file/3192611529",    
   },
   
 ]
